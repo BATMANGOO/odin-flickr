d601a7e1a85b1dadaf311b9d38d9d7b9ebeb46f6
Prabhjot <pjsingh1359@gmail.com>
2021-11-10 22:27:08 -0800

    hid secret key

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,17 @@ class StaticPagesController < ApplicationController
 
   # GET /static_pages or /static_pages.json
   def index
-    flickr = Flickr.new "be0fb3dc27fdb968ab8084518703173d", "c74b9823029ff5f7"
+    flickr = Flickr.new "be0fb3dc27fdb968ab8084518703173d", ENV['FLICKR_SECRET']
 
     if params[:user_id].blank?
       @photos = flickr.photos.getRecent
     else
       @photos = flickr.photos.search(user_id: params[:user_id])
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @photos }
     end
   end
 end
